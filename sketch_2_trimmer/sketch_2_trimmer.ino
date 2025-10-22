c1569ad6d7d7180d115ce920ec101b2dbaffa9f0
chiara620 <chiarabaldo111@gmail.com>
2025-10-22 20:17:11 +0200

    added arduino sketch working with 2 trimmers

---
+++ sketch_2_trimmer/sketch_2_trimmer.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int t0 = analogRead(A0);
+  int t1 = analogRead(A1); 
+
+  // format tipo "A0:valore,A1:valore"
+  Serial.print("A0:");
+  Serial.print(t0);
+  Serial.print(",A1:");
+  Serial.println(t1);
+
+  delay(100);
+}
